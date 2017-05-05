6d7aa85ea513c214519cf1ff068e0c1f69e6567c
ltan <ltan@ualberta.ca.you.used.with.github.com>
2017-05-05 13:43:59 +0000

    Sine Wave again, just for a test

---
+++ SineWave.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    Lambda = input('lambda is:')
+    Lambda = float(Lambda)
+    MySineWave(Lambda)
+
+def MySineWave(Lambda):
+    pi = np.pi
+    x = np.arange(-2*pi, 2*pi, 0.1)
+    k = 2*pi/Lambda
+    y = np.sin(k*x)
+    plt.plot(x, y, 'r-',markersize = 2)
+    plt.xlabel('Position', fontsize=12)
+    plt.ylabel('Amplitude', fontsize=12)
+    #plt.legend('the sine wave',loc='upper left', fontsize=12) # not working
+    plt.title('Sine Wave with Wavelength = %s' % Lambda)
+    plt.show()
+
+main()
